395d5d07e69593df6d755b59954b2f8342883d58
Nacho Villaverde Montalvo <nachovillaverde1@gmail.com>
2016-09-29 10:20:19 +0200

    initial commit

---
+++ HackerBooks2/HackerBooks2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerBooks2
+//
+//  Created by Nacho Villaverde Montalvo on 29/9/16.
+//  Copyright © 2016 vohcan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
